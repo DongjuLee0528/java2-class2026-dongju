f494e46528cc445274c71c4973da70b81d3b94da
DongjuLee <dozoo0528@gmail.com>
2026-04-02 13:50:53 +0900

    5주차 수업

---
+++ src/week05/Ex04_r2.java
@@ -0,0 +1,21 @@
+package week05;
+
+public class Ex04_r2 {
+    public static void main(String[] args) {
+
+
+        for (int i = 2; i <= 9; i++) {
+            System.out.print("[" + i + "단]\t");
+        }
+        System.out.println();
+
+
+        for (int j = 1; j <= 9; j++) {
+            for (int i = 2; i <= 9; i++) {
+                System.out.print(i + "X" + j + "=" + (i * j) + "\t");
+            }
+            System.out.println();
+        }
+
+    }
+}
